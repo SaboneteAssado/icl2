8a9cce5c27cce4ae0fdf4c6b50d6b64c0a5dd84d
Miguel <Miguel@apache-2pe>
2019-12-23 17:33:13 +0000

    last?

--- icl2/src/astNode/ASTLet.java
+++ icl2/src/astNode/ASTLet.java
@@ -17,32 +17,64 @@ import type.Type;
  * @author Miguel Araujo 45699
  *
  */
-public class ASTLet implements ASTNode{
+public class ASTLet implements ASTNode {
 
-	private Map<ASTNode, ASTNode> nodeVals;
-	private ASTNode exp;
+	private List<ASTNode> ids;
+
+	private List<ASTNode> id_values;
+
 	private List<Type> types;
 
-	public ASTLet( Map<ASTNode, ASTNode> nodeVals, ASTNode exp, List<Type> types) {
-		this.nodeVals = nodeVals;
-		this.exp = exp;
-		this.types = types;
+	private ASTNode expr2;
+
+	public ASTLet(List<ASTNode> ids, List<Type> types, List<ASTNode> exprs1, ASTNode expression) {
+		this.ids = ids;
+		this.id_values = exprs1;
+		this.expr2 = expression;
 		this.types = types;
 	}
 
+	/**
+	 * Evaluates a let expression (a declaration). This expression defines identifiers and their values (let part of declaration)
+	 * and then requests a appliance of the same values in an expression (in). 
+	 * This method defines the same identifiers and then evaluates the expression. 
+	 */
 	@Override
-	public IValue eval(EnvironmentAbs<IValue> env) throws Exception {
-		env = env.beginScope();
-
-		Set<ASTNode> keys = nodeVals.keySet();
-		Iterator<ASTNode> it = keys.iterator();
-		while ( it.hasNext() ) {
-			ASTNode key = it.next();
-			env.assoc( ((ASTId) key).getId() , nodeVals.get(key).eval(env));
+	public IValue eval(EnvironmentAbs<IValue> env) {
+		IValue v2 = null;
+		try {
+			env = env.beginScope();
+			for(int i = 0; i < id_values.size(); i++) {
+				IValue v1 = id_values.get(i).eval(env);
+				env.assoc(((ASTId)ids.get(i)).getId(), v1);		 
+			}
+			v2 = expr2.eval(env);
+			env = env.endScope();
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
 		}
-		IValue v = exp.eval(env);
-		env = env.endScope();
-		return v;
+		return v2;
+	}
+
+	/**
+	 * Checks the type of a let construction.
+	 */
+	@Override
+	public Type typeCheck(EnvironmentAbs<Type> env) throws Exception {
+		Type type = null;
+		env = env.beginScope();
+		for (int i = 0; i < ids.size(); i++) {
+			if (types.get(i) instanceof ASTClosureType) {
+				((ASTClosureType)types.get(i)).setEnv(env);
+			}
+			env.assoc(((ASTId)ids.get(i)).getId(), types.get(i));
+			if (!ids.get(i).typeCheck(env).getTypeString().equals(types.get(i).getTypeString())) {
+				throw new Exception("Invalid Type for args of let.");
+			}
+		}
+		type = expr2.typeCheck(env);
+		env.endScope();
+		return type;
 	}
 
 	/**
@@ -91,32 +123,4 @@ public class ASTLet implements ASTNode{
 		code.emit("putfield " + frameid + "/sl Ljava/lang/Object;" );
 		code.emit("astore " + frameid);
 	}
-
-	@Override
-	/**
-	 * checks the type
-	 */
-	public Type typeCheck(EnvironmentAbs<Type> env) throws Exception {
-		Type type = null;
-		env = env.beginScope();
-		for (int i = 0; i < nodeVals.size(); i++) {
-			if (types.get(i) instanceof ASTClosureType) {
-				((ASTClosureType)types.get(i)).setEnv(env);
-			}
-
-
-			Set<ASTNode> keys = nodeVals.keySet();
-			Iterator<ASTNode> it = keys.iterator();
-			while ( it.hasNext() ) {
-				ASTNode key = it.next();
-				if (!key.typeCheck(env).getTypeString().equals(types.get(i).getTypeString())) 
-					throw new Exception("Invalid Type for args of let.");
-				env.assoc( ((ASTId) key).getId() , types.get(i));
-			}
-		}
-		type = exp.typeCheck(env);
-		env.endScope();
-		return type;
-	}
-
 }
